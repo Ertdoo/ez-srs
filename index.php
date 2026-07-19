2c2bb65d2c97b52b5bb0a9d2962c97d0a080424e
Emerson <ezsrs.nixos@gmail.com>
2026-07-19 19:02:28 +1000

    update index.php more buttons

--- index.php
+++ index.php
@@ -8,34 +8,19 @@
         <h2 class="text mb-2">Ahoy there, tis the landing page</h2>
         <p class="text-secondary">Login or register to get yer hands on the plunder</p>
 
-        <?php
-        $card_count = 0;
-        $deck_count = 0;
-        $subject_count = 0;
-
-        if ($result = $conn->query("SELECT COUNT(*) AS cnt FROM cards")) {
-            $card_count = (int) $result->fetch_assoc()['cnt'];
-        }
-        if ($result = $conn->query("SELECT COUNT(*) AS cnt FROM decks")) {
-            $deck_count = (int) $result->fetch_assoc()['cnt'];
-        }
-        ?>
-
         <div class="d-flex justify-content-center gap-5 flex-wrap my-4">
-            <div>
-                <span class="d-block fs-2 fw-bold"><?php echo number_format($card_count); ?></span>
-                <span class="text-uppercase small text-secondary">Cards</span>
-            </div>
-            <div>
-                <span class="d-block fs-2 fw-bold"><?php echo number_format($deck_count); ?></span>
-                <span class="text-uppercase small text-secondary">Decks</span>
-            </div>
+            <a href="land_about.php" class="btn btn-outline-primary px-4">About</a>
+        </div>
+        <br>
+        <div class="d-flex justify-content-center gap-3">
+            <a href="land_register.php" class="btn btn-outline-danger btn-lg px-4">Register</a>
+            <a href="land_login.php" class="btn btn-outline-success btn-lg px-4">Login</a>
+        </div>
+        <br>
+        <div class="d-flex justify-content-center gap-5 flex-wrap my-4">
+            <a href="deck_browse.php" class="btn btn-outline-warning px-4">Explore decks</a>
         </div>
 
-        <div class="d-flex justify-content-center gap-3">
-            <a href="land_register.php" class="btn btn-primary btn-lg px-4">Register</a>
-            <a href="land_login.php" class="btn btn-outline-primary btn-lg px-4">Login</a>
-        </div>
     </main>
 </body>
 <?php include ("footer.php");
